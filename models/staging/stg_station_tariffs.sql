69165bfde546fd78e6309e015b28521795a23ce2
Arun Kumar <11038660@stud.hochschule-heidelberg.de>
2025-05-03 12:04:23 +0000

    fixed parsing issue in stag station tarrif

--- models/staging/stg_station_tariffs.sql
+++ models/staging/stg_station_tariffs.sql
@@ -2,14 +2,14 @@
 {{ config(materialized="view") }}
 
 with
-    src as (
+    raw as (
         select ingest_datetime, json_extract(data_json, '$.tariffs') as tariffs_json
         from {{ source("gbfs", "raw_station_information") }}
     ),
 
     exploded as (
-        select ingest_datetime, t as tariff_json
-        from src, unnest(cast(json_extract_array(tariffs_json) as array<json>)) as t
+        select ingest_datetime, tariff_json
+        from raw, unnest(json_extract_array(tariffs_json)) as tariff_json  -- tariff_json is STRING
     ),
 
     parsed as (
@@ -17,11 +17,11 @@ with
             ingest_datetime,
             json_extract_scalar(tariff_json, '$.tariff_id') as tariff_id,
             json_extract_scalar(tariff_json, '$.name') as tariff_name,
-            cast(
+            safe_cast(
                 json_extract_scalar(tariff_json, '$.cost_per_hour') as float64
             ) as cost_per_hour,
             json_extract_scalar(tariff_json, '$.currency') as currency,
-            cast(
+            safe_cast(
                 json_extract_scalar(tariff_json, '$.duration_minutes') as int64
             ) as duration_minutes
         from exploded
